50bdcbbac67617f5b9c8cfdef852f2e956a2693f
ankushKun <ankush4singh@gmail.com>
2021-10-23 12:34:52 +0530

    first PyPi release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
 # This call to setup() does all the work
 setup(
     name="discordDB",
-    version="0.1.0",
+    version="0.1.1",
     description="CRUD database for discord bots, using discord text channels to store data",
     long_description=long_description,
     long_description_content_type="text/markdown",
